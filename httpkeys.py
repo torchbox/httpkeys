d42b36cabe6fa6f8908f8825bb9a4fe3b80e0d77
Felicity Tarnell <ft@le-Fay.ORG>
2016-05-23 22:30:31 +0100

    use fewer ldap queries for complicated access queries; deduplicate results

--- httpkeys.py
+++ httpkeys.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python
 # vim:sw=4 ts=4 et:
 #
-# Copyright (c) 2015, 2016 Torchbox Ltd.
-# 2015-04-02 ft: created
-# 2016-05-10 ft: modified for TS
+# Copyright (c) 2016 Torchbox Ltd.
+# 2016-05-22 ft: created
 #
 
 from flask import Flask, request, make_response
@@ -25,14 +24,23 @@ def get_ldap_connection(uri):
     conn.start_tls_s()
     return conn
 
-def access_get(conn, acc):
+# Given an access key, return a list of users matching the key.  Key format
+# can be:
+#
+#   username
+#   @groupname
+#   $special:arg1:arg2:...
+#
+# Special keys are defined in settings.
+
+def users_for_access(conn, acc):
     if acc == '':
         return []
 
     if acc[0] == '@':
         # Group lookup
         ret = conn.search_s(
-                    settings.LDAP_GROUP_BASE, 
+                    settings.LDAP_GROUP_BASE,
                     ldap.SCOPE_SUBTREE,
                     settings.LDAP_GROUP_SEARCH_FILTER.format(group = ldap.filter.escape_filter_chars(acc[1:])),
                     [ settings.LDAP_GROUP_MEMBERSHIP_ATTR ])
@@ -41,20 +49,7 @@ def access_get(conn, acc):
         for group in ret:
             users += group[1][settings.LDAP_GROUP_MEMBERSHIP_ATTR]
 
-        filt = "(|" + "".join([ settings.LDAP_UID_SEARCH_FILTER.format(uid = ldap.filter.escape_filter_chars(u)) for u in users ]) + ")"
-
-        ret = conn.search_s(
-                    settings.LDAP_USER_BASE, 
-                    ldap.SCOPE_SUBTREE,
-                    filt,
-                    [ settings.LDAP_KEY_ATTR ])
-
-        keys = []
-        for user in ret:
-            if settings.LDAP_KEY_ATTR in user[1]:
-                keys += user[1][settings.LDAP_KEY_ATTR]
-
-        return keys
+        return users
 
     elif acc[0] == '$':
         # Special lookup
@@ -64,24 +59,52 @@ def access_get(conn, acc):
                 settings.LDAP_USER_BASE,
                 ldap.SCOPE_SUBTREE,
                 settings.LDAP_EXTENDED_FILTERS[parts[0]].format(*parts[1:]),
-                [ settings.LDAP_KEY_ATTR ])
-        keys = []
+                [ settings.LDAP_USERNAME_ATTR ])
+
+        users = []
         for user in ret:
-            if settings.LDAP_KEY_ATTR in user[1]:
-                keys += user[1][settings.LDAP_KEY_ATTR]
-        return keys
+            users += user[1][settings.LDAP_USERNAME_ATTR]
+        return users
     else:
-        # User lookup
-        ret = conn.search_s(
-                    settings.LDAP_USER_BASE, 
-                    ldap.SCOPE_SUBTREE,
-                    settings.LDAP_UID_SEARCH_FILTER.format(uid = ldap.filter.escape_filter_chars(acc)),
-                    [ settings.LDAP_KEY_ATTR ])
-        keys = []
-        for user in ret:
-            if settings.LDAP_KEY_ATTR in user[1]:
-                keys += user[1][settings.LDAP_KEY_ATTR]
-        return keys
+        return [ acc ]
+
+# Given a list of users, return their SSH keys.
+
+def keys_for_users(conn, users):
+    # Construct a search filter matching all users.
+    filt =      \
+        "(|" +  \
+        "".join([
+            settings.LDAP_UID_SEARCH_FILTER.format(
+                uid = ldap.filter.escape_filter_chars(u)
+            ) for u in set(users)
+        ]) +    \
+        ")"
+
+    ret = conn.search_s(
+                settings.LDAP_USER_BASE,
+                ldap.SCOPE_SUBTREE,
+                filt,
+                [ settings.LDAP_KEY_ATTR ])
+
+    keys = []
+
+    for user in ret:
+        if settings.LDAP_KEY_ATTR in user[1]:
+            keys += user[1][settings.LDAP_KEY_ATTR]
+
+    return keys
+
+# Convert a list of accesses into a list of users, and return their
+# SSH keys.
+
+def access_get(conn, acc):
+    users = []
+
+    for access in acc:
+        users += users_for_access(conn, access)
+
+    return keys_for_users(conn, users)
 
 # Look up SSH keys for a list of one or more access list entries.
 
@@ -93,9 +116,7 @@ def lookup():
     access = request.form['access'].split(',')
     conn = get_ldap_connection(settings.LDAP_URI)
 
-    keys = []
-    for acc in access:
-        keys += access_get(conn, acc)
+    keys = access_get(conn, access)
 
     return text_response("\n".join(keys) + "\n")
 
